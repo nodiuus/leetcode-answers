822483a1c7200c61c45536bd2e4049325200144f
Nisan <85966719+nodiuus@users.noreply.github.com>
2023-06-13 22:17:04 -0400

    Update RomanToInteger.cpp

--- RomanToInteger.cpp
+++ RomanToInteger.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <map>
 
+#define string std::string //at this point i just couldnt be asked to LOL
+
+class Solution {
+public:
+     int romanToInt(string String) {
+        std::map<char, int> RomanMap;
+        RomanMap['I'] = 1;
+        RomanMap['V'] = 5;
+        RomanMap['X'] = 10;
+        RomanMap['L'] = 50;
+        RomanMap['C'] = 100;
+        RomanMap['D'] = 500;
+        RomanMap['M'] = 1000;
+
+        int Result = 0;
+
+        for (int i = 0; i < String.length(); i++) {
+            if (i == String.length()) {
+                Result += RomanMap[String[i]];
+            } else if(RomanMap[String[i]] < RomanMap[String[i+1]]) {
+                 Result -= RomanMap[String[i]];
+            } else {
+                 Result += RomanMap[String[i]];
+            }
+
+        }
+        return Result;
+    }
+};
